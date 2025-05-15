62882a2732617cdf23dee4b590e81f448b15d439
L-javeria <bintemurad2000@gmail.com>
2025-05-15 20:20:02 +0500

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import ProductList from '../components/ProductList';
 
 const Home = () => {
-    
-  
     return (
       <>
         <div className='pt-[60px]'>
           <ProductList />
-  
-        </div>
-        
+        </div>    
       </>
     )
 }
